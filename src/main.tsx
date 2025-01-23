86a092e25fed958254bfb826b257258621c647e6
POLLY1515 <amarantepolly2030@gmail.com>
2025-01-23 16:40:52 -0300

    No match route

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css';
-import Expenses from './routes/Expenses/index.tsx';
-import Invoices from './routes/Invoices/index.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Expenses from "./routes/Expenses/index.tsx";
+import Invoices from "./routes/Invoices/index.tsx";
+import NotFound from "./routes/NotFound/index.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App/>}>
-        <Route path='expenses' element={<Expenses />} />
-        <Route path='invoices' element={<Invoices />} />
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="expenses" element={<Expenses />} />
+          <Route path="invoices" element={<Invoices />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
