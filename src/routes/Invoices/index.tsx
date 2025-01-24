24576800a21d7b38fae5d8a6a9743b2313046630
POLLY1515 <amarantepolly2030@gmail.com>
2025-01-24 16:03:05 -0300

    Custom behavior

--- src/routes/Invoices/index.tsx
+++ src/routes/Invoices/index.tsx
@@ -1,6 +1,7 @@
-import { NavLink, Outlet, useSearchParams } from "react-router-dom";
+import {  Outlet, useSearchParams } from "react-router-dom";
 import { getInvoices } from "../../../src/data";
 import './styles.css'
+import QueryLink from "../../components/QueryLink";
 
 export default function Invoices() {
 
@@ -42,13 +43,13 @@ export default function Invoices() {
         })
 
         .map((invoice) => (
-          <NavLink
-          className={({ isActive }) => isActive ? "dblock nav-red" : "dblock nav-blue"} 
+          <QueryLink
+          className={({ isActive }: any) => isActive ? "dblock nav-red" : "dblock nav-blue"} 
             to={`/invoices/${invoice.number}`}
             key={invoice.number}
           >
             {invoice.name}
-          </NavLink>
+          </QueryLink>
         ))}
       </nav>
       <Outlet/>
